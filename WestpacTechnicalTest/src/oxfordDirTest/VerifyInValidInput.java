6d65dc0556d66de9a92133116b9dc252f77e68f7
armandnunez <arman17nunez@gmail.com>
2018-11-27 07:31:46 +0800

    additional files

---
+++ WestpacTechnicalTest/src/oxfordDirTest/VerifyInValidInput.java
@@ -0,0 +1,33 @@
+package oxfordDirTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class VerifyInValidInput {
+	@Test
+	public void Test_Input() throws InterruptedException{
+			
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ArmandoArmandNunez\\EclipseWorkspace\\WestpacTechnicalTest\\lib\\chromedriver.exe");
+	
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.get("https://developer.oxforddictionaries.com/documentation#/");
+			
+			pageLocators searchOxfordDir = new pageLocators(driver);
+			searchOxfordDir.clickCokies();
+			searchOxfordDir.clickExpandOpps();
+			searchOxfordDir.typeInvalidWord();
+			searchOxfordDir.clickTryItBtn();
+			searchOxfordDir.verify404StatusResponse();
+			
+			Thread.sleep(3000);
+			driver.quit();
+			//driver.close();			
+			
+	}	
+
+}
